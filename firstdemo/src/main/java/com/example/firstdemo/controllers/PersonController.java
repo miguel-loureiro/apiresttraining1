cd4f6520889320c23f38a24c052798de2aaf6471
Miguel Loureiro [1181945] <1181945@isep.ipp.pt>
2021-07-14 16:48:50 +0100

    add project files and class diagram

---
+++ firstdemo/src/main/java/com/example/firstdemo/controllers/PersonController.java
@@ -0,0 +1,18 @@
+package com.example.firstdemo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+    @GetMapping
+    public String getPerson() {
+        return "API WORK";
+    }
+    
+}
